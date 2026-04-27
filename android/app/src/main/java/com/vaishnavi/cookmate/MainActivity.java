ff9875ef6c695d60412788559682462bc1c40019
C-Vaishnavi2005 <vaishnavi.csekar@gmail.com>
2026-04-27 18:30:08 +0530

    Added Android app build with Capacitor

---
+++ android/app/src/main/java/com/vaishnavi/cookmate/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vaishnavi.cookmate;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vaishnavi.cookmate',
+  appName: 'CookMate',
+  webDir: 'build'
+};
+
+export default config;
